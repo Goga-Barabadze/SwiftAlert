822be561ed9b0180c1966206096da76e99a0f49a
Goga Barabadze <goga.barabadze73@gmail.com>
2021-04-12 18:39:33 +0200

    Added Alert

--- Sources/SwiftAlert/SwiftAlert.swift
+++ Sources/SwiftAlert/SwiftAlert.swift
@@ -1,3 +1,38 @@
-struct SwiftAlert {
-    var text = "Hello, World!"
+import UIKit
+
+class Alert {
+    
+    static func alert(title: String?, message: String?, target: UIViewController = UIApplication.currentViewController()!, actions: [UIAlertAction] = [UIAlertAction(title: "OK", style: .default, handler: nil)]){
+        
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .actionSheet)
+        
+        if let popoverController = alert.popoverPresentationController {
+            popoverController.sourceView = target.view
+            popoverController.sourceRect = CGRect(x: target.view.bounds.midX, y: target.view.bounds.midY, width: 0, height: 0)
+            popoverController.permittedArrowDirections = []
+        }
+        
+        for action in actions {
+            alert.addAction(action)
+        }
+        
+        target.present(alert, animated: true, completion: nil)
+        
+    }
+    
+}
+
+extension UIApplication{
+
+    class func currentViewController() -> UIViewController? {
+
+        var presentViewController = UIApplication.shared.keyWindow?.rootViewController
+
+        while let pVC = presentViewController?.presentedViewController{
+            presentViewController = pVC
+        }
+
+        return presentViewController
+    }
+
 }
